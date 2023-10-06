3aa1ddd19e4ffa518df83a8d029ffe955e3d6249
LAPTOP-ULCU732A\User <valeri.ignateva@list.ru>
2023-10-06 21:18:01 +0300

    добавлен код

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Произвести расчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "e";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            this.chart1.Location = new System.Drawing.Point(289, 21);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(388, 246);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            title6.Name = "Title1";
+            title6.Text = "(27 - 18x+2x^2)*e^(-x/3)";
+            this.chart1.Titles.Add(title6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(841, 395);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,69 @@ namespace DehotomiaM
         {
             InitializeComponent();
         }
+        double F(double x)
+        {
+            double f;
+            f = (27 - 18 * x + 2 * Math.Pow(x, 2)) * Math.Exp(-(x / 3));
+            return f;
+        }
+        double Proisvodnaya(double x)
+        {
+            double y = -1 * (2 * x * x * Math.Exp(-x / 3) / 3) + 10 * x * Math.Exp(-x / 3) - 27 * Math.Exp(-x / 3);
+            return y;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double Xi = Convert.ToDouble(textBox3.Text);
+            double max;
+            if (F(a) > F(b))
+            {
+                max = F(a);
+            }
+            else
+            {
+                max = F(b);
+            }
+            double answer = 1.0;
+            while (CheckPrecision(Proisvodnaya((a + b) / 2), Xi))
+            {
+                answer = F((double)((a + b) / 2));
+                if (Proisvodnaya((double)((a + b) / 2)) * Proisvodnaya(a) > 0)
+                {
+                    a = (double)((a + b) / 2);
+                }
+                else
+                {
+                    b = (double)((a + b) / 2);
+                }
+            }
+            MessageBox.Show($"минимум {answer}; максимум {max}");
+            this.chart1.Series[0].Points.Clear();
+            double x = 0;
+            double y;
+            while (x <= 8)
+            {
+                y = F(x);
+                this.chart1.Series[0].Points.AddXY(x, y);
+                x += 0.1;
+            }
+        }
+        public static bool CheckPrecision(double yStr, double precision)
+        {
+            bool check = false;
+            string myY = Convert.ToString(yStr);
+            string myPres = Convert.ToString(precision);
+            for (int pos = 2; pos < myPres.Length - 1; ++pos)
+            {
+                if (myY[pos] != '0')
+                {
+                    check = true; break;
+                }
+            }
+            return check;
+        }
     }
 }
